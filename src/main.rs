9f25e56229939e23d71d0c0befe20f472c54db0b
idsbllp <idsbllp@gmail.com>
2021-11-16 22:32:54 +0800

    feat: add ferris_says demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello Rust, are you ok?");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+    println!("Hello, world!");
     println!("Hello, world!");
 }
