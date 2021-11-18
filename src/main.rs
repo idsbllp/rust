d8054f5eee34b52763c628b42b5629a8990fa699
idsbllp <idsbllp@gmail.com>
2021-11-18 22:00:01 +0800

    feat: add types demo

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-mod demo;
+// mod demo;
+// use demo::ferris_says;
+// use demo::rand;
 
-use demo::ferris_says;
-use demo::rand;
+mod types;
+
+use types::number;
+use types::string;
+
+fn test_types() {
+    println!();
+    println!();
+    println!("================== types start ================");
+    number::print();
+    string::print();
+    println!("================== types end ================");
+    println!();
+    println!();
+}
 
 fn main() {
-    ferris_says::print();
-    rand::guess();
+    // ferris_says::print();
+    // rand::guess();
 
-    println!("Hello, world!");
+    test_types();
+
     println!("Hello, world!");
 }
 
