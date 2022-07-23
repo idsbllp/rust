4f22113b8fa85ee549646fec7c31e16f59bfa7fc
idsbllp <idsbllp@gmail.com>
2022-07-23 20:09:27 +0800

    feat: complete error handling demo

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::ErrorKind;
 use std::io::Read;
+use std::io::Error;
 
 fn print1() {
   let file_path = "src/error/hello1.test.md";
@@ -46,10 +47,39 @@ fn print3() {
   File::open(file_path).expect("open file failed");
 }
 
+fn read_file() -> Result<String, Error> {
+  let file_path = "src/error/hello1.test.md";
+  let f = File::open(file_path);
+  
+  let mut f = match f {
+    Ok(f) => f,
+    Err(err) => return Err(err),
+  };
+
+  let mut s = String::new();
+
+  match f.read_to_string(&mut s) {
+    Ok(_) => Ok(s),
+    Err(err) => Err(err),
+  }
+}
+
+fn read_file1() -> Result<String, Error> {
+  let file_path = "src/error/hello1.test.md";
+  let mut f = File::open(file_path)?;
+  let mut s = String::new();
+  f.read_to_string(&mut s)?;
+  Ok(s)
+}
+
 pub fn print() {
   println!("================== error handle start ================");
   print1();
   print2();
   print3();
-  println!("\n================== error handle start ================");
+  let res = read_file();
+  println!("call propagate error result: {:?}", res);
+  let res1 = read_file1();
+  println!("call propagate error 1 result: {:?}", res1);
+  println!("\n================== error handle end ================");
 }
